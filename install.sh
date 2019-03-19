27771c3807f549de722b49b6efe8c458dbd1385c
Molly Lau <windowsbeta@outlook.com>
2019-03-19 13:14:33 +0800

    install v2ray.fun and v2ray main program

---
+++ install.sh
@@ -0,0 +1,197 @@
+#!/bin/bash
+export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+ad_filter="https://raw.githubusercontent.com/ToutyRater/V2Ray-SiteDAT/master/geofiles/h2y.dat"
+
+# 检查是否为Root
+[ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
+
+# 如果检查到运行环境中存在阿里云服务，安装将立即终止
+agentwatch=`ps aux | grep 'agentwatch'`
+aliyunsrv=`ps aux | grep 'aliyun'`
+AliYunDun=`ps aux | grep 'AliYunDun'`
+AliHids=`ps aux | grep 'AliHids'`
+AliYunDunUpdate=`ps aux | grep 'AliYunDunUpdate'`
+if [ -d /usr/local/aegis ] || [[ -f /etc/init.d/aegis ]] || [[ ! -n $agentwatch ]] || [[ ! -n $aliyunsrv ]] || [[ ! -n $AliYunDun ]] || [[ ! -n $AliHids ]] || [[ ! -n $AliYunDunUpdate ]]; then
+    echo -e "[${red}Error${plain}] 检测到您的系统中存在阿里云的相关监控服务进程，如继续使用，你会尝到被社会主义的铁拳打击的滋味！！！"
+    echo "安装程序立即终止"
+    echo "可行的解决办法（以下采用一种即可）："
+    echo "1. 使用我的脚本，卸载阿里云的监控服务：https://github.com/leitbogioro/Fuck_Aliyun"
+    echo "2. 使用我的脚本，重新安装纯净的 Linux 系统：https://github.com/leitbogioro/Tools"
+    rm -rf ssrinstall
+    exit 1
+fi
+
+# 检查系统信息
+if [ -f /etc/redhat-release ];then
+        OS='CentOS'
+    elif [ ! -z "`cat /etc/issue | grep bian`" ];then
+        OS='Debian'
+    elif [ ! -z "`cat /etc/issue | grep Ubuntu`" ];then
+        OS='Ubuntu'
+    else
+        echo "Not support OS, Please reinstall OS and retry!"
+        exit 1
+fi
+
+# 禁用SELinux
+if [ -s /etc/selinux/config ] && grep 'SELINUX=enforcing' /etc/selinux/config; then
+    sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+    setenforce 0
+fi
+
+# 输出 centos 系统大版本号
+System_CentOS=`rpm -q centos-release|cut -d- -f1`
+CentOS_Version=`cat /etc/redhat-release|sed -r 's/.* ([0-9]+)\..*/\1/'`
+
+# Aliyun's depository for CentOS 6
+CentOS6_repo(){
+    yum install wget curl -y
+    mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+    wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-6.repo
+    wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-6.repo
+    yum clean all
+    yum makecache    
+}
+
+# Aliyun's depository for CentOS 7
+CentOS7_repo(){
+    yum install wget curl -y
+    mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
+    wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+    wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
+    yum clean all
+    yum makecache
+}
+
+# Dedicated NodeJS_8.X for CentOS
+nodejs8_CentOS(){
+    curl --silent --location https://rpm.nodesource.com/setup_8.x | bash -
+    yum install -y nodejs
+}
+
+# Dedicated python for CentOS 6
+py_for_centos="https://raw.githubusercontent.com/leitbogioro/Install-python2.7-for-CentOS-6/master/python2.7_for_centos6.sh"
+py_intall="python2.7_for_centos6.sh"
+install_python_for_CentOS6(){
+    yum install wget -y
+    if [ -f ${py_intall} ]; then
+        rm -rf ${py_intall}
+    fi
+    wget ${py_for_centos}
+    if ! wget ${py_for_centos}; then
+        echo -e "[${red}错误${plain}] ${py_file} 下载失败，请检测你的网络！"
+        exit 1
+    fi
+    chmod +x ${py_intall}
+    bash ${py_intall}
+    rm -rf ${py_intall}
+}
+
+# Dedicated pip resource for CentOS 7
+pip_file="get-pip.py"
+pip_url="https://bootstrap.pypa.io/get-pip.py"
+install_python_for_CentOS7(){
+    yum install curl python -y
+    curl ${pip_url} -o ${pip_file}
+    if ! curl ${pip_url} -o ${pip_file}; then
+        echo -e "[${red}错误${plain}] ${pip_file} 下载失败，请检测你的网络！"
+        exit 1
+    fi
+    python ${pip_file}
+    rm -rf ${pip_file}
+}
+
+# Python QRCode depends
+py_qrcode(){
+    pip install pillow
+    pip install qrcode
+}
+
+# Write v2ray process to CentOS6's system service
+vSvc_Path="/etc/init.d/v2ray"
+write_service_to_CentOS6(){
+    wget https://raw.githubusercontent.com/leitbogioro/v2ray.fun/add-on/v2ray
+    if [ -f /etc/init.d/v2ray ];then
+        rm -rf ${vSvc_Path}
+    fi
+    mv v2ray ${vSvc_Path}
+    chmod a+x ${vSvc_Path}
+    chkconfig v2ray on
+    service v2ray start
+}
+
+# Main depends
+if [[ ${OS} == 'CentOS' ]] && [[ ${CentOS_Version} -eq "7" ]]; then
+    CentOS7_repo
+    yum install epel-release vixie-cron socat unzip git ntp ntpdate lrzsz -y
+    nodejs8_CentOS
+    npm install -g qrcode
+    install_python_for_CentOS7
+    py_qrcode
+elif [[ ${OS} == 'CentOS' ]] && [[ ${CentOS_Version} -eq "6" ]]; then
+    CentOS6_repo
+    yum install epel-release vixie-cron socat unzip git ntp ntpdate lrzsz -y
+    nodejs8_CentOS
+    npm install -g qrcode
+    install_python_for_CentOS6
+    py_qrcode
+elif [[ ${OS} == 'CentOS' ]] && [[ ${CentOS_Version} -le "5" ]]; then
+    echo "您的系统版本是（${System_CentOS} ${CentOS_Version}），此系统不受支持，SSR.Go 安装程序即将退出！"
+    exit 1
+else
+    curl -sL https://deb.nodesource.com/setup_8.x | bash -
+	apt-get update
+	apt-get install curl unzip git ntp wget ntpdate python socat lrzsz nodejs -y
+    npm install -g qrcode
+fi
+
+# 安装 acme.sh 以自动获取SSL证书
+curl  https://get.acme.sh | sh
+
+# 克隆V2ray.fun项目
+cd /usr/local/
+rm -R v2ray.fun
+git clone https://github.com/leitbogioro/v2ray.fun
+
+# 安装V2ray主程序
+bash <(curl -L -s https://install.direct/go.sh)
+
+# 配置V2ray初始环境
+ln -sf /usr/local/v2ray.fun/v2ray /usr/local/bin
+chmod +x /usr/bin/v2ray
+chmod +x /usr/local/bin/v2ray
+rm -rf /etc/v2ray/config.json
+cp /usr/local/v2ray.fun/json_template/server.json /etc/v2ray/config.json
+let PORT=$RANDOM+10000
+UUID=$(cat /proc/sys/kernel/random/uuid)
+sed -i "s/UUID_is_here/${UUID}/g" /etc/v2ray/config.json
+sed -i "s/12345/${PORT}/g" "/etc/v2ray/config.json"
+python /usr/local/v2ray.fun/genclient.py
+python /usr/local/v2ray.fun/openport.py
+ad_filter_supplement(){
+    cd /usr/bin/v2ray
+    wget ${ad_filter}
+}
+ad_filter_supplement
+
+# auto open port after start
+# append a new line
+cat /etc/rc.local | grep openport.py
+if [[ $? -ne 0 ]]; then
+cat>>/etc/rc.local<<EOF
+python /usr/local/v2ray.fun/openport.py
+EOF
+chmod a+x /etc/rc.local
+fi
+
+if [[ ${OS} == 'CentOS' ]] && [[ ${CentOS_Version} -eq "6" ]]; then
+    write_service_to_CentOS6
+else
+    service v2ray restart
+fi
+
+clear
+
+echo "V2ray.fun 安装成功！"
+echo "输入 v2ray 回车即可使用"
