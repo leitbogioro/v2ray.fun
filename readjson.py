1fb5ed77649ef0cfd1c1a2f3a2be82dadd581a24
Molly Lau <windowsbeta@outlook.com>
2019-04-19 17:20:09 +0800

    read path

--- readjson.py
+++ readjson.py
@@ -24,6 +24,7 @@ ConfStreamKcpSettings = ConfStream[u'kcpSettings']
 ConfStreamNetwork = ConfStream[u"network"]
 ConfStreamSecurity = ConfStream[u"security"]
 ConfListeningLocal = ConfInbound[u"listen"]
+ConfPath = ConfStream[u"path"]
 
 if config[u"inboundDetour"] and "port" in config[u"inboundDetour"][0]:
     ConfigDynPortRange = config[u"inboundDetour"][0][u"port"]
