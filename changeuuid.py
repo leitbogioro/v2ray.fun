a6898d08856d53593c8a731e5e8bd447c0915cc8
Molly Lau <windowsbeta@outlook.com>
2019-06-01 20:17:22 +0800

    support customize uuid with verification

--- changeuuid.py
+++ changeuuid.py
@@ -1,18 +1,28 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
-import uuid
-import readjson
-import writejson
+import readjson, writejson, uuid
+from utils import is_valid_uuid
 
 print("当前UUID为：%s") % str(readjson.ConfUUID)
 print("是否要随机生成一个新的UUID (Y/n)：")
 genUUID = raw_input()
 
-if genUUID == "y" or genUUID == '':
-    newuuid = uuid.uuid1()
-    print "新的UUID为：{}".format(newuuid)
+def writeuuid(newuuid):
     writejson.WriteUUID(newuuid)
+    print "新的UUID为：{}".format(newuuid)
+
+if genUUID == "y" or genUUID == '':
+    newuuid = uuid.uuid4()
+    writeuuid(newuuid)
 elif genUUID == "n":
-    print("已取消生成新的UUID,未执行任何操作")
+    print("请输入UUID")
+    newuuid = raw_input()
+    if newuuid == "" or is_valid_uuid(newuuid) == False:
+        print("uuid 输入错误")
+        print("")
+        newuuid = uuid.uuid4()
+        writeuuid(newuuid)
+    else:
+        writeuuid(newuuid)
 else:
     print("输入不正确，请输入 y 或 n")
