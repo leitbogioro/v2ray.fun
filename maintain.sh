0326b4ee12664155be1319a5f206acde8fb51940
Molly Lau <windowsbeta@outlook.com>
2019-11-05 21:59:17 +0800

    fix illogic

--- maintain.sh
+++ maintain.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 h2yfile="/usr/bin/v2ray/h2y.dat"
 v2ray_log="/var/log/v2ray"
+v2ray_access_log=${v2ray_log}"/access.log"
+v2ray_error_log=${v2ray_log}"/error.log"
 
 service v2ray stop
 
@@ -10,9 +12,11 @@ fi
 wget https://raw.githubusercontent.com/ToutyRater/V2Ray-SiteDAT/master/geofiles/h2y.dat
 mv h2y.dat ${h2yfile}
 
-if [ -f ${v2ray_log} ]; then
+if [ ! -f ${v2ray_log} ]; then
     rm -rf ${v2ray_log}
     mkdir ${v2ray_log}
+    touch ${v2ray_access_log}
+    touch ${v2ray_error_log}
 fi
 
 service v2ray start
