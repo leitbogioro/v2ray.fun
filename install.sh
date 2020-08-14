ea013deeb732d38ddf35c1bfc4ee3d98129961ed
Molly Lau <windowsbeta@outlook.com>
2020-08-14 22:53:27 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -178,7 +178,8 @@ install_v2ray(){
     bash install-release.sh
     bash install-dat-release.sh
 }
-bash <(curl -L -s https://raw.githubusercontent.com/leitbogioro/v2ray.fun/master/go.sh)
+install_v2ray
+# bash <(curl -L -s https://raw.githubusercontent.com/leitbogioro/v2ray.fun/master/go.sh)
 if [[ ${OS} == 'CentOS' ]] && [[ ${CentOS_Version} -eq "6" ]]; then
     write_service_to_CentOS6
 fi
