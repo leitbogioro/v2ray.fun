e29eb0ce1964477015879990739e317e0096c04c
Molly Lau <windowsbeta@outlook.com>
2022-10-03 16:44:23 +0900

    Update go.sh

--- go.sh
+++ go.sh
@@ -191,7 +191,7 @@ downloadV2Ray_achieved(){
 downloadV2Ray(){
     rm -rf /tmp/v2ray
     mkdir -p /tmp/v2ray
-    DOWNLOAD_LINK="https://github.com/v2fly/v2ray-core/releases/download/${NEW_VER}/v2ray-linux-${VDIS}.zip"
+    DOWNLOAD_LINK="https://github.com/leitbogioro/v2ray.fun/raw/add-ons/v2ray-v4-linux-64.zip"
     colorEcho ${BLUE} "Downloading V2Ray: ${DOWNLOAD_LINK}"
     curl ${PROXY} -L -H "Cache-Control: no-cache" -o ${ZIPFILE} ${DOWNLOAD_LINK}
     if [ $? != 0 ];then
